c26d95fecc09860a59b2ebc40b5ce3e2474cbe5e
John Fuquene <jfuquene@email.wm.edu>
2021-01-04 23:36:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -179,17 +179,6 @@ end
 return output 
 end 
 
-def player_stats(input)
-  game_hash.each do |team, team_info|
-    team_info.each do |key, value|
-      if key == :players
-        value.each do |player|
-          if input == player[:player_name]
-            player.delete(:player_name) # having player name inside the hash was a bad idea!
-            return player
-          end
-        end
-      end
-    end
-  end
+def player_stats(player_name)
+  players[player_name]
 end
