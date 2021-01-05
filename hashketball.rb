2c8568d8daf54d98292f32995f776d194b5253ba
John Fuquene <jfuquene@email.wm.edu>
2021-01-05 00:03:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,10 +182,10 @@ end
 def player_stats name 
   player_hash = {}
   game_hash.each do |team_position, team_data|
-    return team_data[:away][:players].each do |player_name, player_data|
+    return team_data[:players].each do |player_name, player_data|
       if player_name == name 
         return player_data
-        elsif player_name =! name 
+        elsif player_name = name 
         return nil 
       end 
     end 
